5302164df538de2e0e24ef2031da36b4454ac818
logan <gnlee95@gmail.com>
2021-08-19 03:33:43 +0900

    ssac assignment

---
+++ assignment/week2/restart.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Handling Exceptions
+if [ $# -lt 1 ]; then
+    echo "Input Option."
+    echo "Usage: Start < mysql || nginx >"
+    exit 0
+
+elif [ $1 != "mysql" ] && [ $1 != "nginx" ]; then
+    echo "Input correct option"
+    echo "Usage: Start < mysql || nginx >"
+    exit 0
+
+elif [ $# -gt 2 ]; then
+    echo "Choose one or two option(s)."
+    echo "Usage: Start < mysql || nginx >"
+    exit 0
+fi
+
+# mysql --> mysqld
+options=($@)
+cnt=0
+for opt in ${options[@]}
+do 
+    if [ $opt == "mysql" ]; then
+        options[$cnt]="mysqld"
+    fi
+    cnt=$(($cnt+1))
+done
+
+# do start
+docker="ssacdev"
+ncloud="ssac"
+ip=ssach
+
+if [ `uname -n` == $docker ]; then
+    for opt in ${options[@]}
+    do ssh -p 50000 root@$ip "systemctl restart $opt"
+    done
+
+elif [ `uname -n` == $ncloud ]; then
+    for opt in ${options[@]}
+    do systemctl restart $opt
+    done
+
+fi
+
